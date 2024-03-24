8e437a5408465d10c74df43a61e547feeaaa8f70
Karl Newark <karlnewark@Karls-Air.broadband>
2024-03-24 23:57:10 +0000

    changing variant

--- cypress/e2e/binbows/screenshot-test.cy.js
+++ cypress/e2e/binbows/screenshot-test.cy.js
@@ -1,9 +1,17 @@
 describe('Business Card Screenshot Test for binbows', () => {
     it('Visits the home page and takes a screenshot', () => {
-
-        cy.visit('http://127.0.0.1:8080/'); // Adjust this with the path to your project's homepage
-
-        cy.get('body').invoke('attr', 'class', 'binbows');
+        cy.visit('http://127.0.0.1:8080/', {
+            onBeforeLoad(win) {
+                Object.defineProperty(win.navigator, 'userAgent', {
+                    value: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36',
+                    writable: true
+                });
+                Object.defineProperty(win.navigator, 'platform', {
+                    value: 'Win32',
+                    writable: true
+                });
+            },
+        });
 
         cy.wait(10000).compareSnapshot({
             name: 'binbows-loaded',
@@ -14,5 +22,3 @@ describe('Business Card Screenshot Test for binbows', () => {
     });
 
 });
-
-
